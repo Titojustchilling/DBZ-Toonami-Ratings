5e665a78000f8c4cc83873d339c597c693d3396a
Titojustchilling <87554627+Titojustchilling@users.noreply.github.com>
2023-05-30 18:21:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ while quit == 0:
     type in 'exit' if you wish leave the program\n
     """ )
     if whatDoYouWant == 'graph households': 
-        df = pd.read_csv(r'C:\Users\Dell\Downloads\Dragon Ball Z Full Ratings - Sheet1 (1).csv')
+        df = pd.read_csv('data/Dragon Ball Z Full Ratings - Sheet1 (1).csv')
         df.replace('N/A', np.nan, inplace=True)
 
 
@@ -33,7 +33,7 @@ while quit == 0:
         
         
     elif whatDoYouWant == 'graph rank': 
-        df = pd.read_csv(r'C:\Users\Dell\Downloads\Dragon Ball Z Full Ratings - Sheet1 (1).csv')
+        df = pd.read_csv('data/Dragon Ball Z Full Ratings - Sheet1 (1).csv')
         df.replace('N/A', np.nan, inplace=True)
 
 
@@ -61,7 +61,7 @@ while quit == 0:
 
 
             if saga.lower() == 'frieza' or saga.lower() == 'frieza saga':
-                df = pd.read_csv(r'C:\Users\Dell\Downloads\Frieza Saga - Sheet1.csv', usecols=['Sega', 'Episode Titles', 'Household', 'Rank', 'English air date',  'Viewership']) 
+                df = pd.read_csv('data/Frieza Saga - Sheet1.csv', usecols=['Sega', 'Episode Titles', 'Household', 'Rank', 'English air date',  'Viewership']) 
                 rowIndex = userInput - 1
                 episodeTitle = df.loc[rowIndex, 'Episode Titles'].strip()
                 rating = df.loc[rowIndex, 'Household'] 
@@ -70,7 +70,7 @@ while quit == 0:
                 viewership = df.loc[rowIndex, 'Viewership']
                 print(f'Episode {userInput} titled {episodeTitle} received {rating} households with {viewership} viewers watching it on {date}.', end="") 
             elif saga.lower() == 'garlic jr., trunks and androids saga' or saga.lower() == 'garlic jr. trunks and androids saga' or saga.lower() == 'andriods saga' or saga.lower() == 'androids':
-                df = pd.read_csv(r'C:\Users\Dell\Downloads\Garlic Jr. Trunks and Androids Saga - Sheet1.csv', usecols=['Sega ', 'Episode Titles', 'HH', 'Rank ', 'English air date', 'Viewership']) 
+                df = pd.read_csv('data/Garlic Jr. Trunks and Androids Saga - Sheet1.csv', usecols=['Sega ', 'Episode Titles', 'HH', 'Rank ', 'English air date', 'Viewership']) 
                 rowIndex = userInput - 1
                 episodeTitle = df.loc[rowIndex, 'Episode Titles'].strip()
                 rating = df.loc[rowIndex, 'HH'] 
@@ -79,7 +79,7 @@ while quit == 0:
                 viewership = df.loc[rowIndex, 'Viewership'] 
                 print(f'Episode {userInput} titled {episodeTitle} received {rating} households with {viewership} viewers watching it on {date}.', end="") 
             elif saga.lower() == 'imperfect cell and perfect cell sagas' or saga.lower() == 'imperfect cell saga' or saga.lower() == 'perfect cell saga' or saga.lower() == 'imperfect cell and perfect cell saga' or saga.lower() == 'imperfect cell' or saga.lower() == 'perfect cell':
-                df = pd.read_csv(r'C:\Users\Dell\Downloads\Imperfect Cell and Perfect Cell Sagas - Sheet1.csv', usecols=['Sega', 'Episode Titles ', 'English air date ', 'HH', 'Rank', 'Viewership']) 
+                df = pd.read_csv('data/Imperfect Cell and Perfect Cell Sagas - Sheet1.csv', usecols=['Sega', 'Episode Titles ', 'English air date ', 'HH', 'Rank', 'Viewership']) 
                 rowIndex = userInput - 1
                 episodeTitle = df.loc[rowIndex, 'Episode Titles '].strip()
                 rating = df.loc[rowIndex, 'HH'] 
@@ -88,7 +88,7 @@ while quit == 0:
                 viewership = df.loc[rowIndex, 'Viewership'] 
                 print(f'Episode {userInput} titled {episodeTitle} received {rating} households with {viewership} viewers watching it on {date}.', end="") 
             elif saga.lower() == 'cell games' or saga.lower() == 'cell games saga':
-                df = pd.read_csv(r'C:\Users\Dell\Downloads\Cells Games - Sheet1.csv', usecols=['Sega', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership'])
+                df = pd.read_csv('data/Cells Games - Sheet1.csv', usecols=['Sega', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership'])
                 rowIndex = userInput - 1
                 episodeTitle = df.loc[rowIndex, 'Episode Titles'].strip()
                 rating = df.loc[rowIndex, 'HH'] 
@@ -97,7 +97,7 @@ while quit == 0:
                 viewership = df.loc[rowIndex, 'Viewership'] 
                 print(f'Episode {userInput} titled {episodeTitle} received {rating} households with {viewership} viewers watching it on {date}.', end="") 
             elif saga.lower() == 'other world, great saiyaman and world tournament' or saga.lower() == 'other world' or saga.lower() == 'great saiyaman' or saga.lower() == 'world tournament saga':
-                df = pd.read_csv(r'C:\Users\Dell\Downloads\Other World, Great Saiyaman and World Tournament  - Sheet1 (1).csv', usecols=['Sega', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership'])
+                df = pd.read_csv('data/Other World, Great Saiyaman and World Tournament  - Sheet1 (1).csv', usecols=['Sega', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership'])
                 rowIndex = userInput - 1
                 episodeTitle = df.loc[rowIndex, 'Episode Titles'].strip()
                 rating = df.loc[rowIndex, 'HH'] 
@@ -106,7 +106,7 @@ while quit == 0:
                 viewership = df.loc[rowIndex, 'Viewership'] 
                 print(f'Episode {userInput} titled {episodeTitle} received {rating} households with {viewership} viewers watching it on {date}.', end="") 
             elif saga.lower() == 'babidi and majin buu' or saga.lower() == 'majin buu saga' or saga.lower() == 'babidi': 
-                df = pd.read_csv(r'C:\Users\Dell\Downloads\Babidi and Majin Buu - Sheet1.csv', usecols=['Sega', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership '])
+                df = pd.read_csv('data/Babidi and Majin Buu - Sheet1.csv', usecols=['Sega', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership '])
                 rowIndex = userInput - 1
                 episodeTitle = df.loc[rowIndex, 'Episode Titles'].strip()
                 rating = df.loc[rowIndex, 'HH'] 
@@ -115,7 +115,7 @@ while quit == 0:
                 viewership = df.loc[rowIndex, 'Viewership '] 
                 print(f'Episode {userInput} titled {episodeTitle} received {rating} households with {viewership} viewers watching it on {date}.', end="") 
             elif saga.lower() == 'fusion' or saga.lower() == 'fusion, kid buu and peaceful world sagas' or saga.lower() == 'kid buu saga' or saga.lower() == 'peaceful world' or saga.lower() == 'kid buu': 
-                df = pd.read_csv(r'C:\Users\Dell\Downloads\Fusion, Kid Buu and Peaceful World Sagas - Sheet1.csv', usecols=['Saga', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership']) 
+                df = pd.read_csv('data/Fusion, Kid Buu and Peaceful World Sagas - Sheet1.csv', usecols=['Saga', 'Episode Titles', 'Episode air date', 'HH', 'Rank', 'Viewership']) 
                 rowIndex = userInput - 1
                 episodeTitle = df.loc[rowIndex, 'Episode Titles'].strip()
                 rating = df.loc[rowIndex, 'HH'] 
